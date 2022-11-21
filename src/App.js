b79ccc90b91b21cd54d0aa655f6caafbc5eaecfa
Aditya Raj Singh <adi4adityaraj@gmail.com>
2022-11-21 21:14:58 +0530

    Remove Image Gallery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import ImageGallery from "./ImageGallery";
 
 import SearchBox from "./SearchBox";
 
 function App() {
-  // return <ImageGallery />;
   return <SearchBox />;
 }
 
